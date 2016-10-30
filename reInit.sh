30bccc8ce84318dedee48d67d3342c4e56f7e8eb
vernvern <jianhongjianhong2@gmail.com>
2016-10-30 14:18:23 +0800

    asd

--- reInit.sh
+++ reInit.sh
@@ -1,4 +1,40 @@
+#chrome
+apt install wget
+wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+
 apt update
-apt upgrade
+apt upgrade -y 
+
+apt install google-chrome-stable -y
+apt install cmake -y
+sudo apt-get install pylint pylint3 python-dev python3-dev -y
+
+apt install tmux -y
+apt install dpkg -y
+
+cp ~/conf/hosts  /etc/hosts
+mkdir ~/.pip
+cp ~/conf/pip.conf ~/.pip/
+
+#vim
 apt install vim -y
+ln -s ~/conf/vimrc  ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/autoload
+wget https://tpo.pe/pathogen.vim ~/vim/autoload
+git clone https://github.com/kien/rainbow_parentheses.vim ~/.vim/bundle/
+git clone https://github.com/jpalardy/vim-slime ~/.vim/bundle/
+git clone https://github.com/guns/vim-clojure-static ~/.vim/bundle/
+p ~/conf/tmux.conf ~/.tmux.conf 
+
+
+
+
+
+
+
+
+
+
 
